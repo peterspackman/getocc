6d6e76e87ea2405378a2c0991c63dcf8844f7639
Peter Spackman <peterspackman@fastmail.com>
2025-10-20 14:22:45 +0800

    Add CNAME and stuff

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,15 +17,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://getocc.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'peterspackman', // Usually your GitHub org/user name.
+  projectName: 'getocc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
