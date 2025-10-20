cfc35dfb05428919187697283152fcd66eddd001
Peter Spackman <peterspackman@fastmail.com>
2025-10-20 14:33:11 +0800

    Fix React hydration error by loading service worker client-side only

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react';
-import Head from '@docusaurus/Head';
 
 // Root component with COI service worker
 export default function Root({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Head>
-        <script src="/coi-serviceworker.js" />
-      </Head>
-      {children}
-    </>
-  );
+  useEffect(() => {
+    // Load COI service worker only on client side
+    const script = document.createElement('script');
+    script.src = '/coi-serviceworker.js';
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return <>{children}</>;
 }
