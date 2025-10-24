7fb068267563db44bf76a7bc200d29959bc06f89
Peter Spackman <peterspackman@fastmail.com>
2025-10-24 12:45:49 +0800

    Remove static dist JS files, copy from npm package during build

--- static/dist/index.browser.js
+++
@@ -1,7 +0,0 @@
-/**
- * Browser-compatible OCC JavaScript bindings
- * Re-exports from the main index.js without Node.js dependencies
- */
-
-// Re-export everything from the implementation (which includes all WASM exports)
-export * from './index-browser-impl.js';
